41c6a722952ede2bb249c014edc96d87037a7fb1
Scott Rutz <scrutz@microsoft.com>
2021-09-28 14:59:32 -0500

    cp

---
+++ Demo.Function.Api/Demo.Function.Api/Data/ReadingQueryService.cs
@@ -0,0 +1,46 @@
+﻿using Demo.Function.Api.Model;
+
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Function.Api.Data
+{
+    public class ReadingQueryService
+    {
+        private CosmosClient _cosmosClient;
+
+        public ReadingQueryService(CosmosClient cosmosClient)
+        {
+            _cosmosClient = cosmosClient;
+        }
+
+        public async Task<IEnumerable<Reading>> GetAll()
+        {
+            var container = this._cosmosClient.GetContainer("MeasurementDB", "Readings");
+            QueryDefinition query = new QueryDefinition("SELECT * FROM C");
+            List<Reading> results = new List<Reading>();
+            using (FeedIterator<Reading> resultSetIterator = container.GetItemQueryIterator<Reading>(query))
+            {
+                while (resultSetIterator.HasMoreResults)
+                {
+                    Microsoft.Azure.Cosmos.FeedResponse<Reading> response = await resultSetIterator.ReadNextAsync();
+                    results.AddRange(response);
+                }
+            }
+            return results;
+        }
+        public async Task<Reading> GetById(string id, string accountId)
+        {
+            var container = this._cosmosClient.GetContainer("MeasurementDB", "Readings");
+            var result = await container.ReadItemAsync<Reading>(id, new PartitionKey(accountId));
+            return result;
+        }
+    }
+}
---
+++ Demo.Function.Api/Demo.Function.Api/Model/Reading.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo.Function.Api.Model
+{
+    public class Reading
+    {
+        public Reading()
+        { 
+        }
+
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty(PropertyName = "accountId")]
+        public Guid AccountId { get; set; }
+
+        [JsonProperty(PropertyName = "deviceId")]
+        public Guid DeviceId { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public Guid Type { get; set; }
+
+        public TempuratureReading TempuratureReading { get; set; }
+    }
+
+    public class TempuratureReading
+    {
+        public decimal Value { get; set; }
+
+        public string Unit { get; set; }
+    }
+}
